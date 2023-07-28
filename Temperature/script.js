bfe496163d6387e9cb7f4ab48fe56d7d8e5aa680
Isha1814 <135538382+Isha1814@users.noreply.github.com>
2023-07-28 14:30:22 +0530

    isha

--- Temperature/script.js
+++ Temperature/script.js
@@ -1,10 +1,11 @@
-let Celciusinput=document.querySelector('#Celcius > input')
-let Fahrenheitinput=document.querySelector('#Fahrenheit > input')
-let Kelvininput=document.querySelector('#Kelvin > input')
+let Celciusinput=document.querySelector('.Celcius > input')
+let Fahrenheitinput=document.querySelector('.Fahrenheit > input')
+let Kelvininput=document.querySelector('.Kelvin > input')
 let btn=document.querySelector('.button button')
 function roundnumber(number){
     return Math.round(number*100)/100
 }
+/*Celcius to Fahrenheit and Kelvin */
 Celciusinput.addEventListener('input',function(){
     let CTemp = parseFloat(Celciusinput.value)
     let FTemp = (CTemp*(9/5))+32
@@ -12,8 +13,8 @@ Celciusinput.addEventListener('input',function(){
 
     Fahrenheitinput.value = roundnumber(FTemp)
     Kelvininput.value = roundnumber(KTemp)
-
 })
+/*Fahrenheit to Celcius and Kelvin */
 Fahrenheitinput.addEventListener('input',function(){
     let FTemp = parseFloat(Fahrenheitinput.value)
     let CTemp = (FTemp-32)*(5/9)
@@ -23,6 +24,7 @@ Fahrenheitinput.addEventListener('input',function(){
     Kelvininput.value = roundnumber(KTemp)
 
 })
+/*Kelvin to Celcius and Fahrenheit */
 Kelvininput.addEventListener('input',function(){
     let KTemp = parseFloat(Kelvininput.value)
     let CTemp = KTemp-273.15
@@ -30,7 +32,6 @@ Kelvininput.addEventListener('input',function(){
 
     Fahrenheitinput.value = roundnumber(FTemp)
     Celciusinput.value = roundnumber(CTemp)
-
 })
 btn.addEventListener('click', ()=>{
     Celciusinput.value=""
